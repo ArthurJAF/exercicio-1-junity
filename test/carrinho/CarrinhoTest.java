ad10b72cdc443acbec27c9fdca6520e00cfa505f
Arthur <arthurjaf@gmail.com>
2026-08-10 19:43:10 -0300

    Implementando testes para a classe carrinho

--- test/carrinho/CarrinhoTest.java
+++ test/carrinho/CarrinhoTest.java
@@ -3,9 +3,13 @@ package carrinho;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import produto.Produto;
 
 class CarrinhoTest {
 
@@ -15,7 +19,6 @@ class CarrinhoTest {
 	}
 	
 	
-	private Carrinho carr;
 	@BeforeAll
 	static public void print() {
 		System.out.println("Iniciando o teste com 'BeforeAll'");
@@ -24,9 +27,20 @@ class CarrinhoTest {
 	static public void print_after() {
 		System.out.println("Encerrando o teste com AfterAll");
 	}
+	
+	@DisplayName("Teste adiciona item")
 	@Test
 	public void addItem() {
+		Carrinho carrinho = new Carrinho();
+		Produto cpu = new Produto("Ryzen 7", 900.00);
+		Produto gpu = new Produto("RTX 3060", 1600.00);
 		
+		carrinho.addItem(cpu);
+		carrinho.addItem(gpu);
+		
+		Assertions.assertEquals(2, carrinho.getQtdeItems());
+		System.out.println("Rodei o teste 'addItem'");
+
 	}
 
 }
