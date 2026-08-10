4dc371ae1e08d92034e64fa08eb0becb58359f16
Arthur <arthurjaf@gmail.com>
2026-08-10 19:28:34 -0300

    Criação classe CarrinhoTest

---
+++ test/carrinho/CarrinhoTest.java
@@ -0,0 +1,32 @@
+package carrinho;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CarrinhoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+	private Carrinho carr;
+	@BeforeAll
+	static public void print() {
+		System.out.println("Iniciando o teste com 'BeforeAll'");
+	}
+	@AfterAll
+	static public void print_after() {
+		System.out.println("Encerrando o teste com AfterAll");
+	}
+	@Test
+	public void addItem() {
+		
+	}
+
+}
